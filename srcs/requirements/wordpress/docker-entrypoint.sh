d117394754ef0905797d0c28d3d8e5389455b2e2
Arthur Pigeon <pigeon.arthur@hotmail.fr>
2023-02-05 03:26:53 +0100

    wordpress install done, inception mandatory part finished

--- srcs/requirements/wordpress/docker-entrypoint.sh
+++ srcs/requirements/wordpress/docker-entrypoint.sh
@@ -4,4 +4,8 @@ sed -ie "s/\$WORDPRESS_USER/$WORDPRESS_USER/g" /var/www/html/wordpress/wp-config
 sed -ie "s/\$WORDPRESS_PASSWD/$WORDPRESS_PASSWD/g" /var/www/html/wordpress/wp-config.php
 sed -ie "s/\$WORDPRESS_DATABASE/$WORDPRESS_DATABASE/g" /var/www/html/wordpress/wp-config.php
 
+wp core install --path='/var/www/html/wordpress' --url='localhost' \
+  --title="Arthur's Blog" --admin_user='arthur' --admin_password='arthur' \
+  --admin_email='apigeon@student.42.fr' --allow-root
+
 exec $@
