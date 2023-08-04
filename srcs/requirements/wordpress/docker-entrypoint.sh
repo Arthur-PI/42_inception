8d37b959586f9e6c677fcda486a4d997fc671230
Arthur <pigeon.arthur@hotmail.fr>
2023-08-05 01:52:35 +0200

    feat: mariadb all good, need fix on wordpress

--- srcs/requirements/wordpress/docker-entrypoint.sh
+++ srcs/requirements/wordpress/docker-entrypoint.sh
@@ -1,26 +1,42 @@
 #! /bin/bash
+set -e
 
-sed -ie "s/\$MYSQL_USER_PASSWD/$MYSQL_USER_PASSWD/g" /var/www/wordpress/wp-config.php
-sed -ie "s/\$MYSQL_USER/$MYSQL_USER/g" /var/www/wordpress/wp-config.php
-sed -ie "s/\$MYSQL_DATABASE/$MYSQL_DATABASE/g" /var/www/wordpress/wp-config.php
-sed -ie "s/\$MYSQL_HOST/$MYSQL_HOST/g" /var/www/wordpress/wp-config.php
+chown -R $USER:$USER /var/www/wordpress
+sed "s/\$MYSQL_USER_PASSWD/$MYSQL_USER_PASSWD/g" /var/www/wordpress/wp-config.php
+sed "s/\$MYSQL_USER/$MYSQL_USER/g" /var/www/wordpress/wp-config.php
+sed "s/\$MYSQL_DATABASE/$MYSQL_DATABASE/g" /var/www/wordpress/wp-config.php
+sed "s/\$MYSQL_HOST/$MYSQL_HOST/g" /var/www/wordpress/wp-config.php
 
 echo "Installing WordPress website..."
-wp core install --url='apigeon.42.fr' --title="$WORDPRESS_SITE_TITLE" \
-  --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWD" \
-  --admin_email="$WORDPRESS_ADMIN_EMAIL" --allow-root
+wp core install \
+  --allow-root \
+  --url='apigeon.42.fr' \
+  --title="$WORDPRESS_SITE_TITLE" \
+  --admin_user="$WORDPRESS_ADMIN_USER" \
+  --admin_password="$WORDPRESS_ADMIN_PASSWD" \
+  --admin_email="$WORDPRESS_ADMIN_EMAIL" \
 
-wp user create "$WORDPRESS_AUTHOR" "$WORDPRESS_AUTHOR_EMAIL" --role=author --user_pass="$WORDPRESS_AUTHOR_PASSWD" --allow-root
+wp user create "$WORDPRESS_AUTHOR" "$WORDPRESS_AUTHOR_EMAIL" \
+  --allow-root \
+  --role=author \
+  --user_pass="$WORDPRESS_AUTHOR_PASSWD"
 
 echo "Updating WordPress plugins..."
-wp plugin update --all --allow-root
+wp plugin update
+  --allow-root
+  --all
 
 echo "WordPress theme changed to 'twenty twenty two'"
-wp theme install --force --activate twentytwentytwo --allow-root
+wp theme install \
+  --allow-root \
+  --force \
+  --activate twentytwentytwo
 
 # --- BONUS part ---
 echo "Installing and activating the redis-cache plugin..."
-wp plugin install --activate --allow-root redis-cache
+wp plugin install \
+  --activate \
+  --allow-root redis-cache
 
 wp --allow-root plugin install wp-redis
 wp --allow-root redis enable
